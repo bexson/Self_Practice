ada316135b7ad26ee739539ca35d308ddca7d535
Beka <bektemir.abdymomunov@mail.ru>
2021-01-18 13:53:44 -0500

    something changed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
+    public Main(){System.out.println("A "); }//this(10); }
+
+    public Main(int a){
+        this(); // A
+        System.out.println("B "); // A B
+    }
+    public Main(String str){
+        this(2324);
+//        this(100); compilation fails
+        System.out.println(str);
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("HEYYYY");
+        Main obj = new Main("k");
     }
 }
